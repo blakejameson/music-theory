fc6950baa864dcf9d800b6e0067bfd2c6ad91233
blakejameson <lpo13@txstate.edu>
2025-02-22 20:41:18 -0600

    implemented conversion from rare keys to common

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ major_scale_formula = ["W","W","H","W","W","W","H"]
 minor_scale_formula = ["W","H","W","W","H","W","W"]
 minor_scales_that_use_flats = {"D","G","C","F","Ab","Eb","Bb"}
 major_scales_that_use_flats = {"F","Bb","Eb","Ab","Db"}
-major_scales_that_use_sharps = {"F#","B","E","A","D","G","A#","C#","D#","G#"}
+major_scales_that_use_sharps = {"F#","B","E","A","D","G","A#","C#","G#"}
+
+major_keys_that_should_be_converted = {"D#","E#", "A#", "B#", "C#", "G#"}
+
+# D# maj should become Eb maj
+# E# maj should become F major
+# A# maj should become Bb major
+# B# maj should become C major
+# C# maj should become Db major
+# G# maj should be Ab major
+
 
 sharp_to_flat_conversion_map = {
     "A#": "Bb",
     "C#": "Db",
     "D#": "Eb",
     "F#": "Gb",
-    "G#": "Ab"
+    "G#": "Ab",
+    "B#": "C",
+    "E#": "F"
 }
 
 flat_to_sharp_conversion_map = {
@@ -27,11 +39,19 @@ def is_sharp(note: str) -> bool:
     return note[1] == "#"
 
 def whole_step_after_note(current_note: str) -> str:
+    
+    if current_note in flat_to_sharp_conversion_map:
+        current_note = flat_to_sharp_conversion_map[current_note]
+    
     current_note_index_in_music_notes = music_notes.index(current_note)
     whole_step_after = (current_note_index_in_music_notes + 2) % 12
     return music_notes[whole_step_after]
 
 def half_step_after_note(current_note: str) -> str:
+    
+    if current_note in flat_to_sharp_conversion_map:
+        current_note = flat_to_sharp_conversion_map[current_note]
+        
     current_note_index_in_music_notes = music_notes.index(current_note)
     half_step_after = (current_note_index_in_music_notes + 1) % 12
     return music_notes[half_step_after]
@@ -41,6 +61,10 @@ def generate_major_scale(key: str) -> list[str]:
     result = []
     uses_sharps = key in major_scales_that_use_sharps
     current_key = key
+    
+    if current_key in major_keys_that_should_be_converted:
+        current_key = sharp_to_flat_conversion_map[current_key]
+    
     result.append(current_key)
     
     for step in major_scale_formula:
