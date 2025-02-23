55da342a7b670f004e72ad37da25a9c7dd1775ac
blakejameson <lpo13@txstate.edu>
2025-02-22 22:04:41 -0600

    jotted down some of the unusual keys. need to address finding the equivalent flats for sharps

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 music_notes = ["A", "A#","B","C", "C#", "D", "D#", "E", "F","F#", "G", "G#"]
 major_scale_formula = ["W","W","H","W","W","W","H"]
 minor_scale_formula = ["W","H","W","W","H","W","W"]
-minor_scales_that_use_flats = {"D","G","C","F","Ab","Eb","Bb"}
-major_scales_that_use_flats = {"F","Bb","Eb","Ab","Db"}
-major_scales_that_use_sharps = {"F#","B","E","A","D","G","A#","C#","G#"}
 
-major_keys_that_should_be_converted = {"D#","E#", "A#", "B#", "C#", "G#", "Cb", "Fb"}
+major_keys_that_should_be_converted = {"D#","E#", "A#", "B#", "C#", "G#", "Cb", "Fb", "F#"}
 
 # D# maj should become Eb maj
 # E# maj should become F major
@@ -13,7 +10,6 @@ major_keys_that_should_be_converted = {"D#","E#", "A#", "B#", "C#", "G#", "Cb", 
 # B# maj should become C major
 # C# maj should become Db major
 # G# maj should be Ab major
-
 
 major_key_conversion_map = {
     "A#": "Bb",
@@ -25,6 +21,24 @@ major_key_conversion_map = {
     "E#": "F",
     "Cb": "B",
     "Fb": "E"
+}
+
+# Ab minor should be G# minor
+# Cb minor should be B minor
+# Db minor should be C# minor
+# D# minor should be Eb minor
+# Fb minor should be E minor
+# Gb minor should be F# minor
+
+minor_key_conversion_map = {
+    "Ab": "G#",
+    "Cb": "B",
+    "Db": "C#",
+    "D#": "Eb",
+    "Fb": "E",
+    "Gb": "F#"
+    
+    
 }
 
 flat_to_sharp_conversion_map = {
@@ -56,6 +70,30 @@ def half_step_after_note(current_note: str) -> str:
     half_step_after = (current_note_index_in_music_notes + 1) % 12
     return music_notes[half_step_after]
 
+def major_key_uses_flats(key: str):
+    if key == "F":
+        return True
+    if len(key) == 1:
+        return False
+    return key[1] == "b"
+
+def minor_key_uses_flats(key: str):
+    natural_keys_with_flats = {"D","G","C","F"}
+    
+    if key in natural_keys_with_flats:
+        return True
+    
+    if len(key) == 1:
+        return False
+    return key[1] == "b"
+            
+def output_scale_clean(scale: str):
+    print("\t\tThe " + scale[0] + " major scale:")
+    for note in scale:
+        print("%3s   " % (note), end="")
+    print()
+    print()
+        
 
 def generate_major_scale(key: str) -> list[str]:
     result = []
@@ -65,7 +103,7 @@ def generate_major_scale(key: str) -> list[str]:
     if current_key in major_keys_that_should_be_converted:
         current_key = major_key_conversion_map[current_key]
         
-    uses_flats = key_uses_flats(current_key)
+    uses_flats = major_key_uses_flats(current_key)
     
     result.append(current_key)
     
@@ -86,24 +124,19 @@ def generate_major_scale(key: str) -> list[str]:
     
     return result
 
-def key_uses_flats(key: str):
-    if key == "F":
-        return True
-    if len(key) == 1:
-        return False
-    return key[1] == "b"
-            
-def output_scale_clean(scale: str):
-    print("\t\tThe " + scale[0] + " major scale:")
-    for note in scale:
-        print("%3s   " % (note), end="")
-    print()
-    print()
-        
+
 def generate_minor_scale(key: str) -> list[str]:
     result = []
     
     current_key = key
+    
+    
+    if current_key in minor_key_conversion_map:
+        current_key = minor_key_conversion_map[current_key]
+    
+    uses_flats = minor_key_uses_flats(current_key)
+    
+    
     result.append(current_key)
     
     for step in minor_scale_formula:
@@ -111,9 +144,13 @@ def generate_minor_scale(key: str) -> list[str]:
             current_key = whole_step_after_note(current_key)
         else:
             current_key = half_step_after_note(current_key)
+        
+        if uses_flats and len(current_key) == 2 and current_key[1] == "#":
+            current_key = minor_key_conversion_map[current_key]
+            
             
         result.append(current_key)
     return result
 
 for note in music_notes:
-    output_scale_clean(generate_major_scale(note))
+    output_scale_clean(generate_minor_scale(note))
